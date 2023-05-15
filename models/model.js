5f3f510cc3267c3347575e52741c78720c3dfc7a
Neil Rathi <neilrathi@gmail.com>
2023-05-14 21:38:16 -0700

    one model.js to rule them all

---
+++ models/model.js
@@ -0,0 +1,104 @@
+var target = "teacher student book"
+
+// how informative is the alternation?
+var informativity = 'low'
+var numstates = informativity == 'high' ? 6 :
+                informativity == 'med' ? 4 :
+                informativity == 'low' ? 2 : 1
+
+// which type of alternation (T/R)?
+var alternation = 'R'
+
+// cost?
+var useCost = true
+
+// priors?
+var usePriors = false
+
+// generate all alternative states
+var toDict = function(x){ return {string: x} }
+
+var getStates = function(states, numstates) {
+  var numstatesprime = numstates - 1
+  if (numstatesprime != 0 && alternation == 'T') {
+    var newstates = (states + ',teacher student Alt' + numstatesprime).split(',');
+    getStates(newstates, numstatesprime)
+  } else if (numstatesprime != 0 && alternation == 'R') {
+    var newstates = (states + ',teacher Alt' + numstatesprime + ' book').split(',');
+    getStates(newstates, numstatesprime)
+  } else {
+    return map(toDict, states)
+  }
+}
+
+// generate all utterances
+var getUtts = function(states, numstates) {
+  var numstatesprime = numstates - 1
+  if (numstatesprime != 0) {
+    var newstates = (states + ',Alt' + numstatesprime).split(',')
+    getUtts(newstates, numstatesprime)
+  } else {
+    return states
+  }
+}
+
+// creates states, utterances
+var states = getStates(target, numstates)
+var utterances = getUtts('student,book', numstates)
+
+// prior over world states
+var objectPrior = function() {
+  var obj = uniformDraw(states)
+  return obj.string 
+}
+
+var cost = function(utt){
+  useCost ? (utt == "book" ? 0.9 :
+  utt == "student" ? 0.05 : 0) : 0
+}
+
+var prior = function(utt){
+  usePriors ? (utt == "book" ? 0.005244758 :
+  utt == "student" ? 0.032899216 : 0) : 0
+}
+
+// meaning function to interpret the utterances
+var meaning = function(utterance, obj){
+  _.includes(obj, utterance)
+}
+
+// literal listener
+var literalListener = function(utterance){
+  Infer({model: function(){
+    var obj = objectPrior();
+    var uttTruthVal = meaning(utterance, obj);
+    condition(uttTruthVal == true)
+    return obj
+  }})
+}
+
+// set speaker optimality
+var alpha = 1
+
+var util = function(utterance, obj) {
+  alpha * (literalListener(utterance).score(obj) - cost(utterance))
+}
+
+// pragmatic speaker
+var speaker = function(obj) {
+  Infer({model: function(){
+    var utterance = uniformDraw(utterances)
+    factor(alpha * (literalListener(utterance).score(obj) - cost(utterance)))
+    return utterance
+  }})
+}
+
+var getProbs = function(obj, utt1, utt2) {
+  var utt1score = Math.exp(speaker(obj).score(utt1)) // 0.33
+  var utt2score = Math.exp(speaker(obj).score(utt2)) // 0.66
+  var a = usePriors ? Math.exp(utt1score) * prior(utt1) : Math.exp(utt1score)
+  var b = usePriors ? Math.exp(utt2score) * prior(utt2) : Math.exp(utt2score)
+  return [a/(a+b), b/(a+b)]
+}
+
+viz.bar(['DO', 'PD'], getProbs('teacher student book', 'book', 'student'))
