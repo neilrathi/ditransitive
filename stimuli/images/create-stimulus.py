acb3119f950f6bbb8450a2f550f80d2bcea788ed
Neil Rathi <neilrathi@gmail.com>
2023-07-11 21:32:42 -0700

    add sell verbs

--- stimuli/images/create-stimulus.py
+++ stimuli/images/create-stimulus.py
@@ -38,7 +38,28 @@ for verb in all_verbs:
                     new_im.paste(im, (x_offset, 0))
                     x_offset += im.size[0]
 
-                new_im.save(f'{agent}-{recipient}.png')
+                new_im.save(f'{verb}/{agent[:-4]}-{recipient[:-4]}.png')
+    elif verb == 'sell':
+        for agent in os.listdir(f'../characters/{verb}/'):
+            if not agent.endswith('.png'):
+                continue
+            agent_file = f'../characters/{verb}/{agent}'
+            for recipient in sell_recipients:
+                recipient_file = f'../characters/recipient/{recipient}'
+                images = [Image.open(x) for x in [recipient_file, agent_file]]
+                widths, heights = zip(*(i.size for i in images))
+
+                total_width = sum(widths)
+                max_height = max(heights)
+
+                new_im = Image.new('RGB', (total_width, max_height))
+
+                x_offset = 0
+                for im in images:
+                    new_im.paste(im, (x_offset, 0))
+                    x_offset += im.size[0]
+
+                new_im.save(f'{verb}/{agent[:-4]}-{recipient[:-9]}.png')
 
 """
 for verb in all_verbs:
